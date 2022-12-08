4f89c23a887e4da9865fbe35a25aebe0053a634b
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-12-08 17:30:26 +0100

    Makes accesses coalescent

--- assignment3.cu
+++ assignment3.cu
@@ -21,6 +21,24 @@ const unsigned int N = 2048; // This code is designed so N can be of the followi
 const unsigned int BLOCK_SIZE = 128;  
 const float PRECISION = 0.005;
 
+// Computes associativity indexes for the shuffle needed to achieve coalescence
+// Example for size 16
+// 0 8 4 12 2 10 6 14 1 9 5 13 3 11 7 15
+vector<int> get_associativity_indexes(int size) {
+    vector<int> result(size, 0);
+    for (int i = size / 2; i < size; i++) {
+        result[i] = 1;
+    }
+    for (int subsize = 2; subsize < size; subsize *= 2) {
+        for (int i = subsize / 2; i < size; i += subsize) {
+            for (int j = 0; j < subsize / 2; j++) {
+                result[i + j] += N / subsize;
+            }
+        }
+    }
+    return result;
+}
+
 vector<float> create_random_vector(unsigned int size) {
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -48,21 +66,21 @@ void compute_cpu(vector<float>& vector, unsigned int pattern_size) {
     }
 }
 
-__global__ void computation_kernel(float* const vector, int pattern_size) {
+__global__ void computation_kernel(float* const vector, int pattern_size, const int* const indexes) {
     int pattern_position = blockIdx.x * pattern_size;
 
     extern __shared__ float data[];
     
     for (int m = threadIdx.x; m < pattern_size; m += blockDim.x) {
-        data[m] = vector[pattern_position + m];
+        data[indexes[m]] = vector[pattern_position + m];
     }
     
     __syncthreads();
     
     for(int step = 1; step < pattern_size; step *= 2) {
         for (int m = threadIdx.x; m < pattern_size / (step * 2); m += blockDim.x) {
-            int first_index = m * step * 2;
-            int second_index = first_index + step;
+            int first_index = m;
+            int second_index = first_index + pattern_size / (step * 2);
             float first_result = (data[first_index] + data[second_index]) / sqrtf(2);
             float second_result = (data[first_index] - data[second_index]) / sqrtf(2);
             data[first_index] = first_result;
@@ -73,23 +91,29 @@ __global__ void computation_kernel(float* const vector, int pattern_size) {
     
     
     for (int m = threadIdx.x; m < pattern_size; m += blockDim.x) {
-        vector[pattern_position + m] = data[m];
+        vector[pattern_position + m] = data[indexes[m]];
     }
 }
 
-void compute_gpu(vector<float>& vector, unsigned int pattern_size, microseconds* const computation_duration) {
-    float* vector_gpu;
-    unsigned int vector_size_in_bytes = vector.size() * sizeof(float);
-    cudaMalloc(&vector_gpu, vector_size_in_bytes);
-    cudaMemcpy(vector_gpu, vector.data(), vector_size_in_bytes, cudaMemcpyHostToDevice); 
+void compute_gpu(vector<float>& v, const vector<int>& indexes, microseconds* const computation_duration) {
+    float* v_gpu;
+    unsigned int v_size_in_bytes = v.size() * sizeof(float);
+    cudaMalloc(&v_gpu, v_size_in_bytes);
+    cudaMemcpy(v_gpu, v.data(), v_size_in_bytes, cudaMemcpyHostToDevice); 
 
+    int* indexes_gpu;
+    unsigned int indexes_size_in_bytes = indexes.size() * sizeof(int);
+    cudaMalloc(&indexes_gpu, indexes_size_in_bytes);
+    cudaMemcpy(indexes_gpu, indexes.data(), indexes_size_in_bytes, cudaMemcpyHostToDevice); 
+
+    int pattern_size = indexes.size();
     int block_size = pattern_size / 2;
     dim3 dimBlock(block_size > BLOCK_SIZE ? BLOCK_SIZE : (block_size > 32 ? block_size : 32));
-    dim3 dimGrid(vector.size() / pattern_size);
+    dim3 dimGrid(v.size() / pattern_size);
     
     auto start = steady_clock::now();
 
-    computation_kernel<<<dimGrid, dimBlock, pattern_size * sizeof(float)>>>(vector_gpu, pattern_size);
+    computation_kernel<<<dimGrid, dimBlock, pattern_size * sizeof(float)>>>(v_gpu, pattern_size, indexes_gpu);
 
     cudaDeviceSynchronize();
     auto end = steady_clock::now();
@@ -98,9 +122,10 @@ void compute_gpu(vector<float>& vector, unsigned int pattern_size, microseconds*
         *computation_duration = duration_cast<microseconds>(end - start);
     }
 
-    cudaMemcpy(vector.data(), vector_gpu, vector_size_in_bytes, cudaMemcpyDeviceToHost); 
+    cudaMemcpy(v.data(), v_gpu, v_size_in_bytes, cudaMemcpyDeviceToHost); 
 
-    cudaFree(vector_gpu);
+    cudaFree(v_gpu);
+    cudaFree(indexes_gpu);
 }
 
 int main() {
@@ -112,7 +137,7 @@ int main() {
     microseconds computation_duration;
 
     auto start = steady_clock::now();
-    compute_gpu(vector_gpu, N, &computation_duration);
+    compute_gpu(vector_gpu, get_associativity_indexes(N), &computation_duration);
     auto end = steady_clock::now();
 
     microseconds total_duration = duration_cast<microseconds>(end - start);
